bef6c09dd3fd94450bacc2a610003ad05437ebb6
parthishere <parthishere1234@gmail.com>
2024-12-05 03:38:49 -0700

    RTCC and Display remains

--- Keyboard_Firmware/src/EPD_Test.c
+++ Keyboard_Firmware/src/EPD_Test.c
@@ -64,87 +64,87 @@ int EPD_test(void)
     timerWaitUs_polled(1000*2000);
 #endif
 
-#if 1   // Drawing on the image
-    LOG_INFO("Drawing\r\n");
-    //1.Select Image
-    Paint_SelectImage(BlackImage);
-    Paint_Clear(WHITE);
+// #if 1   // Drawing on the image
+//     LOG_INFO("Drawing\r\n");
+//     //1.Select Image
+//     Paint_SelectImage(BlackImage);
+//     Paint_Clear(WHITE);
 
-    // 2.Drawing on the image
-    Paint_DrawPoint(5, 10, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 25, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 40, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
-    Paint_DrawPoint(5, 55, BLACK, DOT_PIXEL_4X4, DOT_STYLE_DFT);
+//     // 2.Drawing on the image
+//     Paint_DrawPoint(5, 10, BLACK, DOT_PIXEL_1X1, DOT_STYLE_DFT);
+//     Paint_DrawPoint(5, 25, BLACK, DOT_PIXEL_2X2, DOT_STYLE_DFT);
+//     Paint_DrawPoint(5, 40, BLACK, DOT_PIXEL_3X3, DOT_STYLE_DFT);
+//     Paint_DrawPoint(5, 55, BLACK, DOT_PIXEL_4X4, DOT_STYLE_DFT);
 
-    Paint_DrawLine(20, 10, 70, 60, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawLine(70, 10, 20, 60, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
-    Paint_DrawLine(170, 15, 170, 55, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
-    Paint_DrawLine(150, 35, 190, 35, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+//     Paint_DrawLine(20, 10, 70, 60, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+//     Paint_DrawLine(70, 10, 20, 60, BLACK, DOT_PIXEL_1X1, LINE_STYLE_SOLID);
+//     Paint_DrawLine(170, 15, 170, 55, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
+//     Paint_DrawLine(150, 35, 190, 35, BLACK, DOT_PIXEL_1X1, LINE_STYLE_DOTTED);
 
-    Paint_DrawRectangle(20, 10, 70, 60, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawRectangle(85, 10, 130, 60, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+//     Paint_DrawRectangle(20, 10, 70, 60, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+//     Paint_DrawRectangle(85, 10, 130, 60, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
 
-    Paint_DrawCircle(170, 35, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
-    Paint_DrawCircle(170, 85, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
-    Paint_DrawString_EN(5, 85, "waveshare", &Font20, BLACK, WHITE);
-    Paint_DrawNum(5, 110, 123456789, &Font20, BLACK, WHITE);
+//     Paint_DrawCircle(170, 35, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_EMPTY);
+//     Paint_DrawCircle(170, 85, 20, BLACK, DOT_PIXEL_1X1, DRAW_FILL_FULL);
+//     Paint_DrawString_EN(5, 85, "waveshare", &Font20, BLACK, WHITE);
+//     Paint_DrawNum(5, 110, 123456789, &Font20, BLACK, WHITE);
 
-    Paint_DrawString_CN(5, 135,"���abc", &Font12CN, BLACK, WHITE);
-    Paint_DrawString_CN(5, 155, "΢ѩ����", &Font24CN, WHITE, BLACK);
+//     Paint_DrawString_CN(5, 135,"���abc", &Font12CN, BLACK, WHITE);
+//     Paint_DrawString_CN(5, 155, "΢ѩ����", &Font24CN, WHITE, BLACK);
 
-    EPD_1IN54_V2_Display(BlackImage);
-    timerWaitUs_polled(1000*2000);
-#endif
+//     EPD_1IN54_V2_Display(BlackImage);
+//     timerWaitUs_polled(1000*2000);
+// #endif
 
-#if 1   //Partial refresh, example shows time    
+// #if 1   //Partial refresh, example shows time    
 
-    // The image of the previous frame must be uploaded, otherwise the
-    // first few seconds will display an exception.
+//     // The image of the previous frame must be uploaded, otherwise the
+//     // first few seconds will display an exception.
     
-    EPD_1IN54_V2_DisplayPartBaseImage(BlackImage);
+//     EPD_1IN54_V2_DisplayPartBaseImage(BlackImage);
 
-    // enter partial mode
-	EPD_1IN54_V2_Init_Partial();
-    LOG_INFO("Partial refresh\r\n");
-    Paint_SelectImage(BlackImage);
-    PAINT_TIME sPaint_time;
-    sPaint_time.Hour = 12;
-    sPaint_time.Min = 34;
-    sPaint_time.Sec = 56;
-    uint8_t num = 10;
-    for (;;) {
-        sPaint_time.Sec = sPaint_time.Sec + 1;
-        if (sPaint_time.Sec == 60) {
-            sPaint_time.Min = sPaint_time.Min + 1;
-            sPaint_time.Sec = 0;
-            if (sPaint_time.Min == 60) {
-                sPaint_time.Hour =  sPaint_time.Hour + 1;
-                sPaint_time.Min = 0;
-                if (sPaint_time.Hour == 24) {
-                    sPaint_time.Hour = 0;
-                    sPaint_time.Min = 0;
-                    sPaint_time.Sec = 0;
-                }
-            }
-        }
-        Paint_ClearWindows(15, 65, 15 + Font20.Width * 7, 65 + Font20.Height, WHITE);
-        Paint_DrawTime(15, 65, &sPaint_time, &Font20, WHITE, BLACK);
-        num = num - 1;
-        if(num == 0) {
-            break;
-        }
-        EPD_1IN54_V2_DisplayPart(BlackImage);
-        // timerWaitUs_polled(1000*500);//Analog clock 1s
-    }
+//     // enter partial mode
+// 	EPD_1IN54_V2_Init_Partial();
+//     LOG_INFO("Partial refresh\r\n");
+//     Paint_SelectImage(BlackImage);
+//     PAINT_TIME sPaint_time;
+//     sPaint_time.Hour = 12;
+//     sPaint_time.Min = 34;
+//     sPaint_time.Sec = 56;
+//     uint8_t num = 10;
+//     for (;;) {
+//         sPaint_time.Sec = sPaint_time.Sec + 1;
+//         if (sPaint_time.Sec == 60) {
+//             sPaint_time.Min = sPaint_time.Min + 1;
+//             sPaint_time.Sec = 0;
+//             if (sPaint_time.Min == 60) {
+//                 sPaint_time.Hour =  sPaint_time.Hour + 1;
+//                 sPaint_time.Min = 0;
+//                 if (sPaint_time.Hour == 24) {
+//                     sPaint_time.Hour = 0;
+//                     sPaint_time.Min = 0;
+//                     sPaint_time.Sec = 0;
+//                 }
+//             }
+//         }
+//         Paint_ClearWindows(15, 65, 15 + Font20.Width * 7, 65 + Font20.Height, WHITE);
+//         Paint_DrawTime(15, 65, &sPaint_time, &Font20, WHITE, BLACK);
+//         num = num - 1;
+//         if(num == 0) {
+//             break;
+//         }
+//         EPD_1IN54_V2_DisplayPart(BlackImage);
+//         // timerWaitUs_polled(1000*500);//Analog clock 1s
+//     }
+// 
+// #endif
 
-#endif
+    // LOG_INFO("Clear...\r\n");
+    // EPD_1IN54_V2_Init();
+    // EPD_1IN54_V2_Clear();
 
-    LOG_INFO("Clear...\r\n");
-    EPD_1IN54_V2_Init();
-    EPD_1IN54_V2_Clear();
-
-    LOG_INFO("Goto Sleep...\r\n");
-    EPD_1IN54_V2_Sleep();
+    // LOG_INFO("Goto Sleep...\r\n");
+    // EPD_1IN54_V2_Sleep();
     free(BlackImage);
     BlackImage = NULL;
 
